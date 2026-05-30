bc8e792ec2494ac1d1a853e852c09057f683b165
Torres92 <aleph2445@gmail.com>
2026-05-30 12:46:12 -0500

    evento mensaje escuchar y emitir listo

---
+++ sockets/socket.ts
@@ -0,0 +1,17 @@
+import socketIO, { Socket } from "socket.io";
+
+export const desconectar = ( cliente: Socket ) => {
+
+    cliente.on('disconnect', () => {
+        console.log(' cliente desconectado ');
+    })
+}
+
+export const mensaje = ( cliente: Socket, io: socketIO.Server ) => {
+
+    cliente.on('mensaje', (payload: { de: string, cuerpo: string }) => {
+        console.log(' Mensaje recibido: ', payload);
+
+        io.emit('mensaje-nuevo', payload);
+    })
+}
